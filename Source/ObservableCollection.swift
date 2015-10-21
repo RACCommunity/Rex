5bce52afa4aee9ca1cf8ca7b35a67a7623982644
Neil Pankey <npankey@gmail.com>
2015-10-21 07:56:31 -0400

    Wrap the observable collection

--- Source/ObservableCollection.swift
+++ Source/ObservableCollection.swift
@@ -14,9 +14,17 @@ public enum CollectionEvent<Collection: CollectionType> {
     case Composite([CollectionEvent])
 }
 
-public protocol ObservableCollectionType: CollectionType {
+public protocol ObservableCollectionType {
     typealias Collection: CollectionType
 
+    /// Read-only access to the underlying collection. These _should_ be copy-on-write
+    /// to avoid a copy on each access while still allowing the callee to update the
+    /// returned collection.
+    ///
+    /// N.B. Investigate clojure vector because CoW is still expensive.
+    var collection: Collection { get }
+
+    /// Safely subscribe to changes on `collection`.
     func observe() -> SignalProducer<(Collection, SignalProducer<CollectionEvent<Collection>, NoError>), NoError>
 }
 
@@ -77,7 +85,11 @@ public final class ObservableArray<Element>: ObservableCollectionType, MutableCo
     }
 
     // ObservableCollectionType
-    
+
+    public var collection: [Element] {
+        return elements
+    }
+
     public func observe() -> SignalProducer<(Collection, SignalProducer<CollectionEvent<Collection>, NoError>), NoError> {
         return SignalProducer { observer, disposable in
             let (producer, sink) = SignalProducer<CollectionEvent<Collection>, NoError>.buffer()
