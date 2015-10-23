59b66fd4abe30e2743bebadf355903e0915db0ff
Neil Pankey <npankey@gmail.com>
2015-10-23 19:25:23 -0400

    Split out CollectionEvent

--- Source/Collections/ObservableCollection.swift
+++ Source/Collections/ObservableCollection.swift
@@ -8,50 +8,6 @@
 
 import ReactiveCocoa
 
-public enum CollectionEvent<Collection: CollectionType> {
-    case Insert(Cursor<Collection>)
-    case Remove(Cursor<Collection>)
-    case Composite([CollectionEvent])
-    
-    public static func insert(element: Collection.Generator.Element, _ index: Collection.Index) -> CollectionEvent {
-        return .Insert(Cursor(element: element, index: index))
-    }
-
-    public static func remove(element: Collection.Generator.Element, _ index: Collection.Index) -> CollectionEvent {
-        return .Remove(Cursor(element: element, index: index))
-    }
-}
-
-extension CollectionEvent: CustomStringConvertible {
-    public var description: String {
-        switch self {
-        case let .Insert(cursor):
-            return "INSERT: \(cursor)"
-        case let .Remove(cursor):
-            return "REMOVE: \(cursor)"
-        case let .Composite(changes):
-            let joined = changes.map { "<\($0.description)>" }.joinWithSeparator(", ")
-            return "{ \(joined) }"
-        }
-    }
-}
-
-public func == <C: CollectionType where C.Generator.Element: Equatable>(lhs: CollectionEvent<C>, rhs: CollectionEvent<C>) -> Bool {
-    switch (lhs, rhs) {
-    case let (.Insert(left), .Insert(right)):
-        return left == right
-    case let (.Remove(left), .Remove(right)):
-        return left == right
-    case let (.Composite(left), .Composite(right)):
-        if left.count == right.count {
-            return zip(left, right).map { $0 == $1 }.reduce(true) { $0 && $1 }
-        }
-        return false
-    default:
-        return false
-    }
-}
-
 public protocol ObservableCollectionType {
     typealias Collection: CollectionType
 
