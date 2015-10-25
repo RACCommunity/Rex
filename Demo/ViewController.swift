9f181489dd049f5924340ea59310cdb55b56fafb
Neil Pankey <npankey@gmail.com>
2015-10-25 11:30:43 -0400

    Stub demo project

---
+++ Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RexDemo
+//
+//  Created by Neil Pankey on 10/25/15.
+//  Copyright © 2015 Neil Pankey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
