67702a2ce478540b846a08de3ad8fe5a7ca341cd
Neil Pankey <npankey@gmail.com>
2015-11-22 17:09:49 -0800

    Fix the change tests since Delta was removed

--- Tests/Collections/ObservableArrayTests.swift
+++ Tests/Collections/ObservableArrayTests.swift
@@ -13,22 +13,34 @@ class ObservableArrayTests: XCTestCase {
 
     func testObservation() {
         var array = ObservableArray<Int>()
-        var patch: Delta<CollectionChange<[Int]>>!
+        var patch: [CollectionChange<[Int]>]!
 
         array.observe().startWithNext { _, patches in
             patches.startWithNext { patch = $0 }
         }
 
         array.insert(5, atIndex: 0)
-        XCTAssert(patch == .Single(.insert(5, atIndex: 0)))
+        assertChangesEqual(patch, [.insert(5, atIndex: 0)])
 
         array.insert(0, atIndex: 1)
-        XCTAssert(patch == .Single(.insert(0, atIndex: 1)))
+        assertChangesEqual(patch, [.insert(0, atIndex: 1)])
 
         array.replaceRange(1..<1, with: [4, 3, 2])
-        XCTAssert(patch == .Batch([.insert(4, atIndex: 1), .insert(3, atIndex: 2), .insert(2, atIndex: 3)]))
+        assertChangesEqual(patch, [.insert(4, atIndex: 1), .insert(3, atIndex: 2), .insert(2, atIndex: 3)])
 
         array.removeAtIndex(0)
-        XCTAssert(patch == .Single(.remove(5, atIndex: 0)))
+        assertChangesEqual(patch, [.remove(5, atIndex: 0)])
+    }
+
+    func assertChangesEqual(actual: [CollectionChange<[Int]>], _ expected: [CollectionChange<[Int]>]) {
+        guard actual.count == expected.count else {
+            XCTFail()
+            return
+        }
+        
+        for (a, e) in zip(actual, expected) {
+            XCTAssert(a == e)
+        }
     }
 }
+
