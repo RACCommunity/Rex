26cdd23d15c5473072c13ee2ef9a9165ff8f70c8
Anders <hello@andersio.co>
2016-07-15 18:53:09 +0800

    Includes the gyb utility.

---
+++ script/pull-gyb.sh
@@ -0,0 +1,6 @@
+mkdir gyb
+curl "https://raw.githubusercontent.com/apple/swift/master/utils/gyb.py" -o "gyb/gyb.py"
+curl "https://raw.githubusercontent.com/apple/swift/master/utils/gyb" -o "gyb/gyb"
+curl "https://raw.githubusercontent.com/apple/swift/master/LICENSE.txt" -o "gyb/LICENSE.txt"
+chmod +x gyb
+
